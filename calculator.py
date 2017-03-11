87ea61f37f29edd79efc8a2bb0ac32e1ef3aeb22
Chris Freeman <chrisfreeman@Chriss-MacBook-Air.local>
2017-03-11 16:21:27 -0500

    adding calculator.py and .gitignore

---
+++ calculator.py
@@ -0,0 +1,32 @@
+import re
+
+print("Welcome to the Magical Python Calculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+#set previous to to zero because we have not done any math yet!
+
+def perform_math():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation: ")
+    else:
+        equation = input(str(previous))
+
+    if equation == 'quit':
+        run = False
+        print("Goodbye, human.")
+    else:
+        equation = re.sub('[a-zA-Z.,:()" "]', '', equation)
+        #eval is dangerous when left to humans, let's make sure only numbers and mathmatical opperators are accepted
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+while run:
+    perform_math()
